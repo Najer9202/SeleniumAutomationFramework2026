fa6fdccc477efff643bd6c9fe01731dfce7a9437
Najer9202 <shaikhnajer19@gmail.com>
2026-09-11 22:07:36 +0530

    Third commit

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,9 +2,17 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+
+import utils.ExtentReportManager;
 import utils.Log;
 
 
@@ -12,6 +20,20 @@ import utils.Log;
 public class BaseTest {
 	
 	protected WebDriver driver;
+	protected static ExtentReports extent;
+	protected ExtentTest test;
+	
+	@BeforeSuite
+	public void setupReport() {
+		extent = ExtentReportManager.getReportInstance();
+	}
+	
+	@AfterSuite
+	public void teardownReport() {
+		extent.flush();
+		//String reportPath = ExtentReportManager.reportPath;
+		//EmailUtils.sendTestReport(reportPath);
+	}
 	@BeforeMethod
 	public void setUp() {
 		Log.info("Setting Up Webdriver");
@@ -22,7 +44,17 @@ public class BaseTest {
 		
 	}
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+		
+               if(result.getStatus() == ITestResult.FAILURE) {
+			
+			String screenshotPath = ExtentReportManager.captureScreenShot(driver, "LoginFailure");
+			test.fail("Test Failed.. Check Screenshot", 
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		}
+		
+		
+		
 		if(driver !=null){
 			Log.info("Closing the browser");
 			driver.quit();
--- src/test/java/tests/LogInTest.java
+++ src/test/java/tests/LogInTest.java
@@ -6,21 +6,29 @@ import org.testng.annotations.Test;
 import base.BaseTest;
 
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class LogInTest extends BaseTest {
 	
 	@Test
 	public void testValidLogIn() throws InterruptedException {
+		
 		Log.info("Starting Login Test");
+		test = ExtentReportManager.createTest("Login Test - ");
+		test.info("Navigating to URL");
 		LoginPage login=new LoginPage(driver);
 		Log.info("Adding Credentails");
+		test.info("Adding Credentails");
 		login.interUsername("admin@yourstore.com");
 		login.interPassword("admin");
+		test.info("Clicking on Login button");
 		login.clickOnLogin();
 		Thread.sleep(5000);
 		Log.info("Validating the title");
-		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+		test.info("Verifying page title");
+		Assert.assertEquals(driver.getTitle(), "Just a moment...a");
+		test.pass("Login Successful");
 		
 	}
 	
