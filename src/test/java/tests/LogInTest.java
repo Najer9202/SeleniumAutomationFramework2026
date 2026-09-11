3980e6a78c35dd0d2de087021abb9b3a91519187
Najer9202 <shaikhnajer19@gmail.com>
2026-09-11 17:39:09 +0530

    added new changes

--- src/test/java/tests/LogInTest.java
+++ src/test/java/tests/LogInTest.java
@@ -4,17 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
+
 import pages.LoginPage;
+import utils.Log;
 
 public class LogInTest extends BaseTest {
 	
 	@Test
 	public void testValidLogIn() throws InterruptedException {
+		Log.info("Starting Login Test");
 		LoginPage login=new LoginPage(driver);
+		Log.info("Adding Credentails");
 		login.interUsername("admin@yourstore.com");
 		login.interPassword("admin");
 		login.clickOnLogin();
-		Thread.sleep(1000);
+		Thread.sleep(5000);
+		Log.info("Validating the title");
 		Assert.assertEquals(driver.getTitle(), "Just a moment...");
 		
 	}
